6c5384d060d1484fa82fa2627888692a34f2332d
b9047753 <b9047753@cmdadmins-mbp.home>
2022-10-30 10:58:51 +0000

    updates on Rmarkdown and renaming of functions

---
+++ R/core/helper_functions/sfc_coord_as_df.R
@@ -0,0 +1,18 @@
+#' @description This function converts an sfc object to a data.frame containing only the 
+#' X and Y coordinates
+#' 
+#' 
+#' @export
+
+
+sfc_coord_as_df <- function(input_sfc) {
+    ## convert to sf  get coordinates in a matrix and then convert to data.frame
+    df <- input_sfc %>% 
+        st_sf() %>% 
+        st_coordinates() %>% 
+        as.data.frame() %>%
+        select(c("X", "Y"))
+    
+    ## Return
+    return(df)
+}
