84bb79374563439cf6e5f739aa9e5eebafcc6c00
b9047753 <b9047753@cmdadmins-mbp.home>
2022-11-09 00:59:20 +0000

    GWUMAP --> Louvain clusters

--- R/examples/examples_gwumap.R
+++ R/examples/examples_gwumap.R
@@ -210,15 +210,16 @@ ggsave(file.path(gwumapDir, paste0(prefix.gwumap, "_embedding.tiff")),
        dpi = 400)
 ## Manually group, plot and map ----
 umap.map.3 <- umap.layout_df.3 %>%
-    mutate("group" = ifelse(.data$V2 > 0, "1", 
-                            ifelse(.data$V2 < 0 & .data$V2 > -6, "3", 
-                                   ifelse(.data$V2 < -5 & .data$V2 > -7.5 & .data$V1 < 0.6, "2", "4")))) %>%
+    mutate("group" = ifelse(.data$V2 > 0 & .data$V2 < 2.5 & .data$V1 < 1.8 & .data$V1 > 0.5, "5",
+                            ifelse(.data$V2 > 0, "1", 
+                                   ifelse(.data$V2 < 0 & .data$V2 > -6, "3", 
+                                          ifelse(.data$V2 < -8.2 & .data$V1 > -0.8 & .data$V1 < 0.4, "2", "4"))))) %>%
     mutate("geometry" = polygons$geom_pol)
 
 ggplot() + 
     geom_point(data = umap.map.3,
                aes(x = V1, y = V2, colour = group)) + 
-    scale_color_manual(values = c4a("wright25", 4)) +
+    scale_color_manual(values = c4a("wright25", 7)) +
     labs(title = "Scaled VST data UMAP",
          subtitle = "n neighbours = 7\nmin dist = 0.05") + 
     my_theme
@@ -231,7 +232,7 @@ ggsave(file.path(gwumapDir, paste0(prefix.gwumap, "_embedding.grouped.tiff")),
 ggplot() + 
     geom_sf(data = umap.map.3$geometry,
             aes(fill = umap.map.3$group)) + 
-    scale_fill_manual(values = c4a("wright25", 4)) +
+    scale_fill_manual(values = c4a("wright25", 7)) +
     labs(title = "Scaled VST data UMAP",
          subtitle = "n neighbours = 7\nmin dist = 0.05") + 
     my_theme
@@ -305,6 +306,43 @@ ggraph::ggraph(g = umap.map.3.ig,
     geom_node_point(aes(colour = grp)) + 
     scale_colour_manual(values = c4a("wright25", 7))
 
+### Cluster with Louvain ----
+umap.map.3.clst <- cluster_louvain(umap.map.3.ig)
+membership(umap.map.3.clst)
+plot(umap.map.3.clst, umap.map.3.ig)
+V(umap.map.3.ig)$louv <- as.factor(membership(umap.map.3.clst))
+
+ggraph::ggraph(g = umap.map.3.ig,
+               layout = "stress") + 
+    geom_edge_link(width = 0.1, edge_colour = "grey66") + 
+    geom_node_point(aes(colour = louv)) + 
+    scale_colour_manual(values = c4a("wright25", 9))
+
+ggraph::ggraph(g = umap.map.3.ig,
+               layout = "manual",
+               x = umap.custom.3$layout[,1],
+               y = umap.custom.3$layout[,2]) + 
+    geom_edge_link(width = 0.1, edge_colour = "grey66") + 
+    geom_node_point(aes(colour = louv)) + 
+    scale_colour_manual(values = c4a("wright25", 9))
+
+umap.map.3 <- umap.map.3 %>%
+    mutate("louvain" = as.factor(membership(umap.map.3.clst)))
+
+ggplot() + 
+    geom_sf(data = umap.map.3$geometry,
+            aes(fill = umap.map.3$louvain)) + 
+    scale_fill_manual(values = c4a("wright25", 9)) +
+    labs(title = "Scaled VST data UMAP",
+         subtitle = "n neighbours = 7\nmin dist = 0.05",
+         fill = "Louvain clusters") + 
+    my_theme
+ggsave(file.path(gwumapDir, paste0(prefix.gwumap, "_embedding.map.louvain.tiff")),
+       width = grDevices::dev.size(units = "in")[1],
+       height = grDevices::dev.size(units = "in")[2],
+       units = "in",
+       dpi = 400)
+
 ## Apply spatial weights to UMAP layout ----
 ### Make sf point object ----
 umap.map.3.cntd <- umap.custom.3$layout %>% # retrieve the UMAP layout
@@ -323,32 +361,32 @@ polygons[1:6,1:3]
 umap.map.3.dist <- gw.dist(dp.locat = st_coordinates(umap.map.3.cntd),
                            p = 2)
 range(umap.map.3.dist)
+
 ### Calculate weighted distances ----
-# Here we will use the neighbours calculation from polygons object to calculate 
-# the spatial weights. Near in space locations will get higher weights.
 dist.Mat <- gw.dist(dp.locat = st_coordinates(polygons$geom_cntd),
                     p = 2)
 bw = 3*spot_diameter(spatialDir)
-kernel = "bisquare"
+kernel = "gaussian"
 
 polygons.wdist <- gw.weight(vdist = dist.Mat, 
                             bw = bw,
                             kernel = kernel,
                             adaptive = FALSE)
 range(polygons.wdist)
+range(dist.Mat)
 
 ### Calculate weighted UMAP distances
-umap.map.3.wdist <- umap.map.3.dist * polygons.wdist
+umap.map.3.wdist <- umap.map.3.dist * dist.Mat
 
-umap.map.3.cmdscale <- cmdscale(umap.map.3.wdist, k = 2, add = TRUE) 
-umap.map.3.weighted <- umap.map.3.cmdscale[["points"]] %>%
+umap.map.3.cmdscale <- cmdscale(umap.map.3.wdist, k = 2, add = TRUE) %>%
+    .[["points"]] %>%
     as.data.frame() %>%
     mutate(group = umap.map.3$group) %>%
     rename("UMAP1" = "V1", "UMAP2" = "V2")
 
 ### Plot the weighted UMAP layout ----
 ggplot() + 
-    geom_point(data = umap.map.3.weighted,
+    geom_point(data = umap.map.3.cmdscale,
                aes(x = UMAP1, y = UMAP2, colour = group)) + 
     scale_color_manual(values = c4a("wright25", 7)) +
     labs(title = "Geographically weighted UMAP",
@@ -360,20 +398,7 @@ ggsave(file.path(gwumapDir, paste0(prefix.gwumap, "_embedding.gwumap.tiff")),
        units = "in",
        dpi = 400)
 
-### Plot log2-transformed weighted UMAP layout ----
-# NOT WORKING!!!
-# ggplot() + 
-#     geom_point(data = umap.map.3.weighted,
-#                aes(x = log2((UMAP1/10^6)+10^4), y = log2((UMAP2/10^6)+10^4), colour = group)) + 
-#     scale_color_manual(values = c4a("wright25", 7)) +
-#     labs(title = "Geographically weighted UMAP",
-#          subtitle = "n neighbours = 7\nmin dist = 0.05") + 
-#     my_theme
-# ggsave(file.path(gwumapDir, paste0(prefix.gwumap, "_embedding.gwumap-log2.tiff")),
-#        width = grDevices::dev.size(units = "in")[1],
-#        height = grDevices::dev.size(units = "in")[2],
-#        units = "in",
-#        dpi = 400)
+### Reconstruct UMAP from point distances ----
 cmdscale <- cmdscale(umap.map.3.dist, k = 2, add = TRUE) %>%
     .[["points"]] %>%
     as.data.frame() %>%
@@ -383,21 +408,64 @@ ggplot() +
     geom_point(data = cmdscale,
                aes(x = UMAP1, y = UMAP2, colour = group)) +
     scale_color_manual(values = c4a("wright25", 7)) +
-    labs(title = "Recostructed UMAP from point distances",
+    labs(title = "Reconstructed UMAP from point distances",
          subtitle = "n neighbours = 7\nmin dist = 0.05") +
     my_theme
 
-cmdscale_polygons <- cmdscale(polygons.wdist, k = 2, add = TRUE) %>%
+### Reconstruct tissue map from weighted point distances ----
+cmdscale_polygons.w <- cmdscale(polygons.wdist, k = 2, add = TRUE) %>%
     .[["points"]] %>%
     as.data.frame() %>%
     mutate(group = umap.map.3$group) %>%
-    rename(cmdscale_polygons, "X" = "UMAP1", "Y" = "UMAP2")
+    rename("X" = "V1", "Y" = "V2")
 ggplot() +
-    geom_point(data = cmdscale_polygons,
+    geom_point(data = cmdscale_polygons.w,
                aes(x = X, y = Y, colour = group)) +
     scale_color_manual(values = c4a("wright25", 7)) +
-    labs(title = "Recostructed map from point distances",
-         subtitle = "kernel = exponential") +
+    labs(title = "Reconstructed map from \nweighted point distances",
+         subtitle = "kernel = bisquared") +
     my_theme
-# ---------------------------------------------------------------------------- #
 
+### Reconstruct tissue map from simple point distances ----
+cmdscale_polygons <- cmdscale(dist.Mat, k = 2, add = TRUE) %>%
+    .[["points"]] %>%
+    as.data.frame() %>%
+    mutate(group = umap.map.3$group) %>%
+    rename("X" = "V1", "Y" = "V2")
+ggplot() +
+    geom_point(data = cmdscale_polygons,
+               aes(x = X, y = Y, colour = group), size = 4.5) +
+    scale_color_manual(values = c4a("wright25", 7)) +
+    labs(title = "Reconstructed map from point distances") +
+    my_theme
+
+## Calculate knn from UMAP layout ----
+umap.custom.3.knn <- knearneigh(umap.custom.3$layout, k = 6)
+
+# ---------------------------------------------------------------------------- #
+# 8. Run UMAP - custom.4 - Scaled VST data ----
+## Customise configuration ----
+custom.config.4 <- umap.defaults
+custom.config.4$n_neighbors <- 50
+custom.config.4$min_dist <- 0.05
+## Run UMAP ----
+umap.custom.4 <- umap(inputUMAP.scaled,
+                      config = custom.config.4)
+
+umap.layout_df.4 <- as.data.frame(umap.custom.4$layout)
+head(umap.layout_df.4)
+polygons[1:6, 1:2]
+## Plot UMAP ----
+ggplot() + 
+    geom_point(data = umap.layout_df.4,
+               aes(x = V1, y = V2)) +
+    labs(title = "Scaled VST data UMAP",
+         subtitle = "n neighbours = 50\nmin dist = 0.05") + 
+    my_theme
+
+prefix.gwumap <- "gwumap_cust.n7.dist005_500.vst.unscaled"
+ggsave(file.path(gwumapDir, paste0(prefix.gwumap, "_embedding.tiff")),
+       width = grDevices::dev.size(units = "in")[1],
+       height = grDevices::dev.size(units = "in")[2],
+       units = "in",
+       dpi = 400)
