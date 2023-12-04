77ce3a02141f147260056b7526c669ef975b6a19
b9047753 <b9047753@cmdadmins-MBP.broadband>
2023-12-04 11:22:09 +0000

    moving from STExplorerDev to STExplorer

--- R/deprecated/updt.R
+++
@@ -1,8 +0,0 @@
-#' a function to update an existing object
-#' 
-
-`updt<-` <- function(x, ..., value){
-  ## x is the object to be manipulated, value the object to be assigned
-  x[... ,] <- value
-  x
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# STExplorer
-A package to analyse and explore Spatial Transcriptomics data based on geocomputational methods of spatial data analysis.
